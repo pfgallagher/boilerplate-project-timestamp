05940700e39f35ccfa6e2e4d6bd2a40602b70fed
pfgallagher <pfgallagher@gmail.com>
2019-04-14 20:23:55 -0500

    Fix bug regarding empty string

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 app.get("/api/timestamp/:dateString", (req, res, next) => {
 	let date = req.params.dateString;
 	if (/^\d{1,}$/g.test(date)) date = parseInt(date, 10);
-	if (!date) {
+	if (date === "") {
 		const now = new Date();
 		res.json({
 			unix: now.getTime(),
